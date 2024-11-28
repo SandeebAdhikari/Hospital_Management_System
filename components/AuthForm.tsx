7b0f5638d01155d4e016490032ff0cd0acdf1f6d
Sandeeb Adhikari <sa2734@njit.edu>
2024-11-28 09:34:57 -0500

    change on color and background

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,13 +22,13 @@ const AuthForm: React.FC<AuthFormProps> = ({
 }) => {
   return (
     <div
-      className={`flex p-5 items-center justify-center w-[400px] ${
+      className={`flex p-5 items-center justify-center w-[400px] shadow-white/35 shadow-md ${
         type === "signIn" ? "h-[373px]" : "h-[418px]"
-      } bg-[#161D2F] rounded-[20px]`}
+      }  rounded-[20px]`}
     >
       <form onSubmit={onSubmit} className="flex flex-col w-full p-4">
         <h2 className="mt-8 text-[32px] text-white">
-          {type === "signIn" ? "Login" : "Sign Up"}
+          {type === "signIn" ? "Sign In" : "Sign Up"}
         </h2>
 
         {type === "signUp" && (
@@ -39,7 +39,7 @@ const AuthForm: React.FC<AuthFormProps> = ({
             value={formData.email || ""}
             onChange={onChange}
             required
-            className="w-[336px] h-[37px] mt-10 p-2 border-b border-[#5A698F] bg-[#161D2F] outline-none focus:ring-0 text-white hover:border-b-white hover:cursor-pointer"
+            className="w-[336px] h-[37px] mt-10 p-2 border-b border-[#5A698F] bg-transparent outline-none focus:ring-0 text-white hover:border-b-white hover:cursor-pointer"
           />
         )}
         <input
@@ -51,7 +51,7 @@ const AuthForm: React.FC<AuthFormProps> = ({
           required
           className={`w-[336px] h-[37px] ${
             type === "signIn" ? "mt-10" : "mt-6"
-          } p-2 border-b border-[#5A698F] bg-[#161D2F] outline-none focus:ring-0 text-white hover:border-b-white hover:cursor-pointer`}
+          } p-2 border-b border-[#5A698F] bg-transparent outline-none focus:ring-0 text-white hover:border-b-white hover:cursor-pointer`}
         />
         <input
           type="password"
@@ -60,12 +60,12 @@ const AuthForm: React.FC<AuthFormProps> = ({
           value={formData.password}
           onChange={onChange}
           required
-          className="w-[336px] h-[37px] mt-6 p-2 border-b border-[#5A698F] bg-[#161D2F] outline-none focus:ring-0 text-white hover:border-b-white hover:cursor-pointer"
+          className="w-[336px] h-[37px] mt-6 p-2 border-b border-[#5A698F] bg-transparent outline-none focus:ring-0 text-white hover:border-b-white hover:cursor-pointer"
         />
 
         <button
           type="submit"
-          className="mt-6 bg-[#FC4747] text-white p-2 w-[336px] h-[48px] rounded hover:bg-white hover:text-black"
+          className="mt-6 bg-transparent shadow-white/35 shadow-sm text-white p-2 w-[336px] h-[48px] rounded hover:bg-white hover:text-black"
         >
           <span className="text-[15px] font-light">
             {type === "signIn" ? "Login to your account" : "Create an account"}
@@ -78,7 +78,7 @@ const AuthForm: React.FC<AuthFormProps> = ({
               Don&apos;t have an account?{" "}
               <Link
                 href="/signUp"
-                className="text-[#FC4747] hover:cursor-pointer"
+                className="text-gray-400  hover:cursor-pointer"
               >
                 Sign Up
               </Link>
@@ -88,9 +88,9 @@ const AuthForm: React.FC<AuthFormProps> = ({
               Already have an account?{" "}
               <Link
                 href="/signIn"
-                className="text-[#FC4747] hover:cursor-pointer"
+                className="text-gray-400 hover:cursor-pointer"
               >
-                Login
+                Sign In
               </Link>
             </>
           )}
